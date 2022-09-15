b76327307bef8e7938f57502d0bf9dbe574ff114
Genaleen <geranteeusebiojr@gmail.com>
2022-09-15 05:36:18 -0800

    doyou

---
+++ DoYou/bouncing.js
@@ -0,0 +1,13 @@
+/* Toggles the style class on click */
+
+$("button").click(function() {
+    $(".button").toggleClass("buttonLeft");
+});
+var button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  var changeTop = Math.random() * ($(window).height() - $("button").height());
+  var changeLeft = Math.random() * ($(window).width() - $("button").width());
+  $("button").css("margin-top", changeTop + "px");
+  $("button").css("margin-left", changeLeft + "px");
+});
